3a792663b3a23cd465a69e1b65ad5e455c4690b2
Nathan Jiudice <98543918+NathanJRS@users.noreply.github.com>
2026-01-30 14:37:11 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("fleeButton");
+const startScreen = document.getElementById("startScreen");
+const passwordScreen = document.getElementById("passwordScreen");
+const finalScreen = document.getElementById("finalScreen");
+
+const passwordInput = document.getElementById("passwordInput");
+const confirmPassword = document.getElementById("confirmPassword");
+
+let clickCount = 0;
+
+// Botão fujão 😈
+button.addEventListener("click", () => {
+
+  clickCount++;
+
+ if (clickCount < 6) {
+
+    alert("Ops, não vai ser tão fácil assim! Tente clicar de novo.");
+
+    const x = Math.random() * (window.innerWidth - button.offsetWidth);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight);
+
+    button.style.position = "absolute";
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+  } else {
+    startScreen.classList.add("hidden");
+    passwordScreen.classList.remove("hidden");
+  }
+});
+
+// Defina a senha aqui 👇
+const correctPassword = "jubileu";
+
+confirmPassword.addEventListener("click", () => {
+  if (passwordInput.value === correctPassword) {
+    passwordScreen.classList.add("hidden");
+    finalScreen.classList.remove("hidden");
+  } else {
+    alert("Hmm… tenta de novo, vai uma dica extra >> Eu falo essa palavra/Nome pra me referir ao meu amiguinho 😅❤️");
+  }
+});
